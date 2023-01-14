759ae55f01bd3e71c72d85577da7e2b804f9b419
zaikunzhang <zaikunzhang@gmail.com>
2023-01-14 10:06:46 +0800

    correct setting of PATH in GITHUB_ENV

--- intelvars_unix.sh
+++ intelvars_unix.sh
@@ -4,4 +4,4 @@
 source /opt/intel/oneapi/setvars.sh
 ifort --version
 where ifort
-echo PATH='$PATH':"$(dirname "$(command -v ifort)")" >> "$GITHUB_ENV"
+echo PATH="$PATH":"$(dirname "$(command -v ifort)")" >> "$GITHUB_ENV"
